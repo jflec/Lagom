c3905e5e0b7784785463815a4352bb2e8825ad9d
JFlec <josephflec@gmail.com>
2021-10-27 19:04:03 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import './utilities/_variables.scss';
 import './App.scss';
 import './reset.scss';
+import './navbar/navbar.scss';
+import Navbar from './navbar/navbar';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div id="grid">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/navbar/navbar.js
@@ -0,0 +1,26 @@
+const Navbar = () => {
+  return (
+    <div id="navbar">
+      <h1>Lagom</h1>
+      <ul id="navbar-container">
+        <li className="navbar-item">
+          <button href="#">FILLER</button>
+        </li>
+        <li className="navbar-item">
+          <button href="#">FILLER</button>
+        </li>
+        <li className="navbar-item">
+          <button href="#">FILLER</button>
+        </li>
+        <li className="navbar-item">
+          <button href="#">FILLER</button>
+        </li>
+        <li className="navbar-item">
+          <button href="#">FILLER</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
