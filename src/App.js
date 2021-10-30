c8c3edcdbf0731f41af9c251ea0ef83cdc8c3bcb
JFlec <josephflec@gmail.com>
2021-10-29 19:05:25 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './utilities/_variables.scss';
 import './App.scss';
 import './reset.scss';
 import './splash/scene.scss';
+import './splash/splash.scss';
 
 import Scene from './splash/scene';
+import Splash from './splash/splash';
 
 function App() {
   return (
     <div id="App">
+      <Splash />
       <Scene />
     </div>
   );
---
+++ src/splash/splash.js
@@ -0,0 +1,12 @@
+const Splash = () => {
+  return (
+    <div id="splash">
+      <h1 id="splash-name">
+        Joe <br></br>
+        Felicidario
+      </h1>
+    </div>
+  );
+};
+
+export default Splash;
